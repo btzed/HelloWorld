a2e8ef519afc13f50d70cf073eae552ab1e2efc7
Zed <494751861@qq.com>
2015-02-28 14:11:03 +0800

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Zed on 15/2/28.
+//  Copyright (c) 2015年 BT Zed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
